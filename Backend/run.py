d2a720a21d5a9ebf9afa0a326fec457cb357b3d0
anshu3769 <at3769@nyu.edu>
2019-03-20 14:41:17 -0400

    comments added

--- Backend/run.py
+++ Backend/run.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
+"""
+This module creates the flask application and
+runs it.
 
-from data import database as db
-from data import schema
+"""
 from flask import Flask
 from flask_graphql import GraphQLView
 from flask_cors import CORS
+from data import database as db
+from data import schema
 
 
 def create_app():
-    app = Flask(__name__)
-    app.debug = True
-    app.add_url_rule('/graphql',
-                  view_func=GraphQLView.as_view(
-                     'graphql',
-                      schema=schema.schema,
-                      graphiql=True))
+    """
+    This method creates flask app
 
+    Input  : None
+    Output : Flask application
+
+    """
+    flask_app = Flask(__name__)
+    flask_app.debug = True
+    flask_app.add_url_rule(
+        "/graphql",
+        view_func=GraphQLView.as_view(
+            "graphql",
+            schema=schema.schema,
+            graphiql=True),
+    )
 
     @app.teardown_appcontext
     def shutdown_session(exception=None):
@@ -23,12 +35,12 @@ def create_app():
 
     return app
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     db.init_db()
 
     app = create_app()
-    CORS(app, resources={r'/graphql': {'origins': '*'}})
+    CORS(app, resources={r"/graphql": {"origins": "*"}})
 
-    app.run(host='0.0.0.0')
-
+    app.run(host="0.0.0.0")
