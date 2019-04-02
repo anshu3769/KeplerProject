fa5f4b7bb83aeaa49cff2aec0bc926546d88e076
anshu3769 <at3769@nyu.edu>
2019-04-02 08:06:18 -0400

    test query added

--- Backend/data/tests/test_query.py
+++ Backend/data/tests/test_query.py
@@ -25,16 +25,6 @@ def test_player_lastnames_query(snapshot):
     """
     snapshot.assert_match(client.execute(query))
 
-def test_words_query(snapshot):
-    query= """
-        query {
-            words {
-                word
-            }
-        }
-    """
-    snapshot.assert_match(client.execute(query))
-
 
 def test_player_by_firstname_query(snapshot):
     query= """
@@ -57,3 +47,27 @@ def test_player_by_lastname_query(snapshot):
         }
     """
     snapshot.assert_match(client.execute(query))
+
+
+def test_words_query(snapshot):
+    query= """
+        query {
+            words {
+                word
+            }
+        }
+    """
+    snapshot.assert_match(client.execute(query))
+
+
+def test_top_scores_query(snapshot):
+    query= """
+        query {
+            topScores {
+                userName
+                value
+            }
+        }
+    """
+    snapshot.assert_match(client.execute(query))
+
